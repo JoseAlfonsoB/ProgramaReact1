f46d5c53caf33188c2d241c3c157f01b46bda6f0
JoseAlfonsoB <jalfonsobautista74@gmail.com>
2025-03-27 09:42:51 -0600

    Agregamos un contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { useState } from 'react';
+
 const App = ()=>{
+
+  const [contador , setContador] = useState(0);
+
+  const eventoBotonContador = (incremento)=>{
+    setContador(contador + incremento)
+  }
   return(
-    <div>App</div>
+    <div>
+      <h1 >Contador: {contador}</h1>
+      <button onClick={()=> eventoBotonContador(1)}>Disminuir</button>
+      <button onClick={()=> eventoBotonContador(-1)}>Disminuir</button>
+    </div>
   )
 }
 
