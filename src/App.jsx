6f2b15d6977cb8321a38188267570bf00f70cdeb
JoseAlfonsoB <jalfonsobautista74@gmail.com>
2025-03-27 09:58:01 -0600

    Se renderizo un parrafo para el condicional si el contador es par o impar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 
+//* Todo lo que se encuentra dentro de la función de "App" es lo que se va a renderizar en el navegador. En otras palabras es el contenido que se va a mostrar en la página web.
 const App = ()=>{
-
+  //* Todo lo que se encuentra dntro de la función de "App" se exscribe en JavaScript XML (JSX). Y lo que este afuera de esta sera escrito en JavaScript puro.
   const [contador , setContador] = useState(0);
 
   const eventoBotonContador = (incremento)=>{
@@ -12,6 +13,12 @@ const App = ()=>{
       <h1 >Contador: {contador}</h1>
       <button onClick={()=> eventoBotonContador(1)}>Disminuir</button>
       <button onClick={()=> eventoBotonContador(-1)}>Disminuir</button>
+      <br />
+      {
+        contador % 2 == 0?
+        (<p>El contador es par</p>) :
+        (<p>El contador es impar</p>)
+      }
     </div>
   )
 }
